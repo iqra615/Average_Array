37dbb4525c4b2641bda131676b016e0c3a6c809b
HP 6th <iqra615>
2020-06-24 05:18:58 -0700

    Array_Java

---
+++ src/come/codewithiqra/Main.java
@@ -0,0 +1,16 @@
+package come.codewithiqra;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double [] array={3,4,5,6,7,8,9,344444,5,6,7,8,9,588,8,8,6,4,3,2,77,447};
+	double total = 0;
+
+	for (int i = 0; i<array.length; i++)
+    {
+        total = total+i;
+    }
+	double average = total/array.length;
+        System.out.printf("The Average of:.%3f ", average);
+    }
+}
